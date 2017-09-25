8bfd04af57e4d9fccbd1904fa55c4aacd3283322
AsiaIv0918 <104051067@live.asia.edu.tw>
2017-09-25 14:11:38 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+ public class Main {
+
+    public static void main(String[] args) {
+	Scanner input=new Scanner(System.in);
+	int num1, num2, num3, sum;
+	System.out.print("請輸入一個整數");
+	num1=input.nextInt();
+	System.out.print("請再輸入一個整數");
+	num2=input.nextInt();
+	System.out.print("請再再輸入一個整數");
+	num3=input.nextInt();
+	sum = num1+num2-num3;
+	System.out.printf("%d+%d-%d=%d",num1 ,num2 , num3 ,sum);
+	input.close(); //此行可打可不打
+    }
+}
